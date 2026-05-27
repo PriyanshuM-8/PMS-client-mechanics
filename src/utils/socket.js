c2325e400a4133b6ed24687087f31f23f23bb79a
priya8957-dell <maddeshiyapriyanshu2@gmail.com>
2026-05-27 20:02:16 +0530

    OTP problem

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -15,7 +15,7 @@ export const initiateSocket = (userId) => {
     socket = null
   }
 
-  socket = io('/', {
+  socket = io(import.meta.env.VITE_SOCKET_URL || '/', {
     reconnection: true,
     reconnectionAttempts: 10,
     reconnectionDelay: 2000,
